4e8002c1bd9056eb53b2d1411e3a0fbfa53df423
MaruBlosso <accccv01@gmail.com>
2025-09-07 17:28:07 +0900

    feature/backend-initial 소스 최초 등록

---
+++ src/main/java/com/marublosso/worktrack/worktrack_backend/WorktrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marublosso.worktrack.worktrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktrackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marublosso/worktrack/worktrack_backend/WorktrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marublosso.worktrack.worktrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
